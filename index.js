4956f0f9ff7b594e458bd1a262a10f3f03ac4f33
Davi Ferreira <daviferreiraaew@gmail.com>
2023-05-28 23:49:02 -0300

    ultima atualização

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const express = require('express');
-
 const app = express();
-
 app.use(express.json());
-
 app.get('/', (request, response) => {
-  response.send('Seja, bem vindo!');
+  response.send('Seja, bem-vindo!');
 });
-
-app.get("/users", (request, response) => {
-  
-  response.send("Você chamou o endpoint:/users pelo metodo get")
+app.get('/users', (request, response) => {
+  response.send('Você chamou o endpoint:/users pelo método GET');
 });
-
-app.post("/products", (request, response) => {
-  const { name, price, description } = request.body;
-
-  const message = `Você chamou o endpoint:/products pelo método POST<br>Produto: ${name}.<br>Preço: ${price}.<br>Descrição: ${description}`;
-
+app.post('/products', (request, response) => {
+  const {name, price, description} = request.body;
+  const message = `Endpoint:/products pelo método POST<br>Produto:
+   ${name}.<br>Preço: ${price}.<br>Descrição: ${description}`;
   response.send(message);
 });
-
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
