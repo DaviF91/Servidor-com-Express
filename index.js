1f2dee231ac4ba7ad373bd17082b0ffa65f56d25
Davi Ferreira <daviferreiraaew@gmail.com>
2023-05-28 22:30:33 -0300

    novos arquivos

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-  response.send('Seja, bem vindo');
+  response.send('Seja, bem vindo!');
+});
+
+app.get("/users", (request, response) => {
+  
+  response.send("Você chamou o endpoint:/users pelo metodo get")
+});
+
+app.post("/products", (request, response) => {
+  const { name, price, description } = request.body;
+
+  const message = `Você chamou o endpoint:/products pelo método POST<br>Produto: ${name}.<br>Preço: ${price}.<br>Descrição: ${description}`;
+
+  response.send(message);
 });
 
 const PORT = 3333;
